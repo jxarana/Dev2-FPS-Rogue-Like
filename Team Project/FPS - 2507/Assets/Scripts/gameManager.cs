d7acdfe58cd0738e7123f9eac41d635ba5bbf677
jxarana <JXArana@student.fullsail.edu>
2025-07-08 12:09:18 -0400

    Created small shop Added shop stand and Added weapon models to Unity Store DL

--- Team Project/FPS - 2507/Assets/Scripts/gameManager.cs
+++ Team Project/FPS - 2507/Assets/Scripts/gameManager.cs
@@ -11,7 +11,7 @@ public class gameManager : MonoBehaviour
     [SerializeField] GameObject menuWin;
     [SerializeField] GameObject menuLose;
     [SerializeField] GameObject menuUnlocks;
-    [SerializeField] GameObject menuShop;
+    public GameObject menuShop;
 
     public Image ammoBar;
     public Image playerHPBar;
@@ -155,6 +155,13 @@ public class gameManager : MonoBehaviour
     {
         statePause();
         menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
+    public void openShop()
+    {
+        statePause();
+        menuActive = menuShop;
         menuActive.SetActive(true);
     }
 }
--- Team Project/FPS - 2507/Assets/Scripts/openShop.cs
+++ Team Project/FPS - 2507/Assets/Scripts/openShop.cs
@@ -4,28 +4,17 @@ using UnityEngine;
 
 public class openShop : MonoBehaviour
 {
-    public GameObject shopRogueLike;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        shopRogueLike = GameObject.Find("Shop");
-        shopRogueLike.SetActive(false);
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            shopRogueLike.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            shopRogueLike.SetActive(false);
+            gameManager.instance.openShop();
         }
     }
 }
